06e5f50ca2cacf5bb82f4ed061206591eca3e76a
Asa HP Choi <asa_hp_choi@manulife.com>
2018-05-15 17:17:58 +0800

    POC

--- ngapp/src/app/report-chart/report-chart.component.ts
+++ ngapp/src/app/report-chart/report-chart.component.ts
@@ -53,6 +53,20 @@ export class ReportChartComponent implements OnInit {
     console.log(this.barData)
   }
 
+  saveSVG() {
+    var svgEl = document.getElementById("svg");
+    svgEl.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    var svgData = svgEl.outerHTML;
+    var preface = '<?xml version="1.0" standalone="no"?>\r\n';
+    var svgBlob = new Blob([preface, svgData], { type: "image/svg+xml;charset=utf-8" });
+    var svgUrl = URL.createObjectURL(svgBlob);
+    var downloadLink = document.createElement("a");
+    downloadLink.href = svgUrl;
+    downloadLink.download = name;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  }
   colors = ['#FF5D55',
     '#FF8C86',
     '#00AA59',
