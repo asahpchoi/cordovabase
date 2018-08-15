fddd692a2be0d1e09fd2fe96762e7621dd46b638
Asa HP Choi <asa_hp_choi@manulife.com>
2018-08-15 14:59:11 +0800

    X

--- ngapp/src/app/autocomplete/autocomplete.component.ts
+++ ngapp/src/app/autocomplete/autocomplete.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {MatChipInputEvent} from '@angular/material';
+import { MatChipInputEvent } from '@angular/material';
+import { Http } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-autocomplete',
@@ -10,18 +12,59 @@ export class AutocompleteComponent implements OnInit {
   items = [];
   searchText;
   selectItem;
-  selectItems=[];
+  selectItems = [];
   selecting = false;
   labels;
+  fullList;
+  searching = false;
 
+  sel = {
+    searchText: ''
+  };
+
+  addressmaster = {};
+
+  getMatchItems() {
+   if(!this.fullList || this.sel.searchText == '') return;
+ 
+    return this.fullList.filter(
+      x => x.display.toLowerCase().includes(this.sel["searchText"].toLowerCase())
+    ).filter((x,i) => i<10)
+  }
+
+  getPDisplay(code) {
+    return this.addressmaster["provinces"].find(
+      x => x.value == code
+    ).display
+  }
+
+  getDDisplay(code, provinceId) {
+    return this.addressmaster["districts"].find(
+      x => x.value == code && provinceId == x.provinceId
+    ).display
+  }
+
+  getdistricts() {
+    if(!this.addressmaster["districts"]) return;
+    return this.addressmaster["districts"].filter(
+      x => x.provinceId == this.sel["p"]
+    );
+
+  }
+  getwards() {
+    if(!this.addressmaster["wards"]) return;
+    return this.addressmaster["wards"].filter(
+      x => x.provinceId == this.sel["p"] && x.districtId == this.sel["d"]
+    );
+  }
   delete(i) {
     let idx = this.selectItems.indexOf(i);
     this.selectItems.splice(idx, 1);
   }
   pick(i) {
-    
+
     this.selectItems.push(i);
-    this.selecting=false;
+    this.selecting = false;
 
   }
   select() {
@@ -35,21 +78,29 @@ export class AutocompleteComponent implements OnInit {
     )
   }
 
-  constructor() { 
- 
+  constructor(private http: HttpClient) {
+
   }
 
   loaditems() {
     return Object.keys(this.labels)
-    .filter(x => !this.selectItems.includes(x)
-    )
-    .filter(x=>this.labels[x].toLowerCase().includes(this.searchText.toLowerCase()))
-    .filter(
-      (x,i) => i < 5
-    )
+      .filter(x => !this.selectItems.includes(x)
+      )
+      .filter(x => this.labels[x].toLowerCase().includes(this.searchText.toLowerCase()))
+      .filter(
+        (x, i) => i < 5
+      )
   }
 
   ngOnInit() {
+    this.http.get('/assets/address.json').subscribe(
+      d => {
+        //console.log(d)
+        this.addressmaster = d;
+        this.fullList = [...this.addressmaster["provinces"], ...this.addressmaster["districts"], ...this.addressmaster["wards"]];
+      }
+    )
+
     this.labels = {
       "OCC0001": "Administration - Accountant",
       "OCC0002": "Administration - Actuary",
